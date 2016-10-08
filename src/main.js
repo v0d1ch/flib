c4cf742c2be491ba10843c82651cc00b53b430e8
Sasa Bogicevic <bogicevicsasa@yahoo.com>
2016-10-08 16:48:53 +0200

    Add immutube methods for practise

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 (function () {
-	var R = require('ramda');
+	var _ = require('ramda');
 	document.write("Bacon practise<br />");
+
+	var eventValue = compose(_.get('value'), _.get('target'));
+
+	//+ valueStream :: DomEvent -> EventStream String
+	var valueStream = _.compose(map(eventValue), listen('keyup'));
+
+	var inputStream = _.compose(map(termToUrl), valueStream);
 
 	function extractValFromEvent(prop, event){
 			return event.prop;
@@ -13,11 +20,11 @@
 	function setElementEvent(element, event){
 		 return Bacon.fromEvent(document.getElementById(element), event);
 	}
-	var bindToElement = setElementEvent('username','keyup');
+	//var bindToElement = setElementEvent('username','keyup');
 	//console.log(bindToElement);
 	// var bind = R.curry(setElementByIdValue('test'), extractValFromEvent('target.value'));
 	// bind(bindToElement);
-	// Bacon.fromEvent(document.getElementById("username"), "keyup").onValue(function(event){
-	// 	console.log(event.target.value);
-	// });
+	 Bacon.fromEvent(document.getElementById("username"), "keyup").onValue(function(event){
+	 	document.getElementById("test").innertext = event.target.value;
+	 });
 })()
