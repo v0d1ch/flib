37caf315e21468af4e50edc63b5a12251a0616f4
Sasa Bogicevic <bogicevicsasa@yahoo.com>
2016-10-09 23:37:26 +0200

    Trying to catch event stream

--- src/main.js
+++ src/main.js
@@ -1,30 +1,31 @@
 (function () {
-	var _ = require('ramda');
-	document.write("Bacon practise<br />");
+	'use strict';
 
-	var eventValue = compose(_.get('value'), _.get('target'));
+	var R = require('ramda');
+	var log = function(x) { console.log(x); return x;  };
 
-	//+ valueStream :: DomEvent -> EventStream String
-	var valueStream = _.compose(map(eventValue), listen('keyup'));
+	var eventValue = R.compose(R.prop('value'), R.prop('target'));
 
-	var inputStream = _.compose(map(termToUrl), valueStream);
+	var listen = R.curry(function (event, target) {
+		return Bacon.fromEvent(document.getElementById(target), event).onValue(function(evt){
+			return eventValue(evt);
+		});
+	});
 
-	function extractValFromEvent(prop, event){
-			return event.prop;
-	}
+	var attachToElm = R.curry(function(elm, val){
+		document.getElementById(elm).innerText = val;
+	});
 
-	function setElementByIdValue(element, value){
-		document.getElementById(element).value = value;
-	}
+	var valueStream = R.compose(R.map(eventValue), listen('keyup'));
 
-	function setElementEvent(element, event){
-		 return Bacon.fromEvent(document.getElementById(element), event);
-	}
-	//var bindToElement = setElementEvent('username','keyup');
-	//console.log(bindToElement);
-	// var bind = R.curry(setElementByIdValue('test'), extractValFromEvent('target.value'));
-	// bind(bindToElement);
-	 Bacon.fromEvent(document.getElementById("username"), "keyup").onValue(function(event){
-	 	document.getElementById("test").innertext = event.target.value;
-	 });
-})()
+	var inputStream = valueStream('username');
+
+	var showBindedValues = R.compose(R.map(attachToElm),log, valueStream);
+
+	showBindedValues('test');
+	console.log(eventValue);
+
+	// Bacon.fromEvent(document.getElementById("username"), "keyup").onValue(function(event){
+		// 	document.getElementById("test").innerText = event.target.value;
+	// });
+})();
