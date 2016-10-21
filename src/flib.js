2ff07dd410dc12d5edf18eeaf0ff7d148ab9061e
Sasa Bogicevic <bogicevicsasa@yahoo.com>
2016-10-21 23:19:55 +0200

    Try to implement Maybe

---
+++ src/flib.js
@@ -0,0 +1,50 @@
+(function(){
+
+	'use strict';
+
+
+	var Maybe = function(value) {
+		var Nothing = {
+			bind: function(fn) {
+				return this;
+			},
+			isNothing: function() {
+				return true;
+			},
+			val: function() {
+				throw new Error("cannot call val() nothing");
+			},
+			maybe: function(def, fn) {
+				return def;
+			},
+			toString: function(){
+				return 'Nothing';
+			}
+		};
+
+		var Just = function(value) {
+			return {
+				bind: function(fn) {
+					return Maybe(fn.call(this, value));
+				},
+				isNothing: function() {
+					return false;
+				},
+				val: function() {
+					return value;
+				},
+				maybe: function(def, fn) {
+					return fn.call(this, value);
+				},
+				toString: function(){
+					return 'Just';
+				}
+			};
+		};
+
+		if (typeof value === 'undefined' || value === null)
+			return Nothing;
+
+		return Just(value);
+	};
+})();
