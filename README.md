defb5ac6a6cc1ebccd07ba3ee40ee7d87512cae0
Sasa Bogicevic <bogicevicsasa@yahoo.com>
2016-10-26 20:08:05 +0200

    Update maybe example

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
 ###fLib - functional helper library
 :: JS implementations of constructs from functional world that make your life easier in everyday work with JS
+
 Inspired by Haskell computational patterns
 
 ##Available constructs
  - Maybe
+
+ ```
+
+	var f = require('./flib');
+
+	var a = {
+		'key1':'a',
+		'key2':'b'
+	};
+
+
+	var b = f.Maybe(a.key2);
+	b.isNothing(); // -> false
+
+	var b = f.Maybe(a.key3);
+	b.isNothing(); // -> true , key3 does not exist in object a 
+ 
+ 
+ ``` 
--- src/main.js
+++ src/main.js
@@ -2,33 +2,15 @@
 
 	'use strict';
 
-	var _ = require('./flib');
-	var R = require('ramda');
+	var f = require('./flib');
 
-	// var eventValue = R.compose(R.prop('value'), R.prop('target'));
+	var a = {
+		'key1':'a',
+		'key2':'b'
+	};
 
-	// var listen = R.curry(function (event, target) {
-	// 	return Bacon.fromEvent(document.getElementById(target), event).onValue(function(evt){
-	// 		return eventValue(evt);
-	// 	});
-	// });
+	var b = ['a','b','c','d'];
 
-	// var attachToElm = R.curry(function(elm, val){
-	// 	document.getElementById(elm).innerText = val;
-	// });
-
-	// var valueStream = R.compose(R.map(eventValue), listen('keyup'));
-
-	// var inputStream = valueStream('username');
-
-	// var showBindedValues = R.compose(R.map(attachToElm),log, valueStream);
-
-	// showBindedValues('test');
-	console.log(_);
-	var a = _.Maybe(3);
-	console.log(a.toString());
-
-	// Bacon.fromEvent(document.getElementById("username"), "keyup").onValue(function(event){
-		// 	document.getElementById("test").innerText = event.target.value;
-// });
+	var c = f.Maybe(a.key2);
+	console.log(c.isNothing());
 })();
