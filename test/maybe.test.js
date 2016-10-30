ebc09244c20290ece28a2f40ac4079143c385a8c
Sasa Bogicevic <bogicevicsasa@yahoo.com>
2016-10-30 16:52:50 +0100

    Add more tests for Just

--- test/maybe.test.js
+++ test/maybe.test.js
@@ -3,6 +3,8 @@ var chai = require('../node_modules/chai/lib/chai');
 var assert = chai.assert;
 var expect = chai.expect;
 
+// Nothing
+
 describe('Nothing.toString()', function(){
 	it('.toString() if called on null should return Nothing', function() {
 		var test = F.Maybe(null).toString();
@@ -51,6 +53,59 @@ describe('Nothing.bind()', function(){
 		assert.equal(test.val(), 2);
 	});
 });
+
+// JUST
+
+describe('Just.toString()', function(){
+	it('.toString() if called on 1 should return Just', function() {
+		var test = F.Maybe(1).toString();
+		assert.equal(test, 'Just');
+	});
+});
+
+describe('Just.val()', function(){
+	it('.val() if called on 1 should return 1', function() {
+		var test = F.Maybe(1);
+		assert.equal(test.val(),1);
+	});
+});
+
+describe('Just.hasValue()', function(){
+	it('.hasValue() called on null should return false', function() {
+		var test = F.Maybe(null);
+		expect(test.hasValue()).to.not.be.true;
+	});
+
+	it('Just.hasValue() called on number should return true', function() {
+		var test = F.Maybe(1);
+		expect(test.hasValue()).to.be.true;
+	});
+});
+
+describe('Just.maybe()', function(){
+	it('.maybe() called on null should return default value', function() {
+		var test = F.Maybe(null).maybe('', F.id);
+		assert.equal(test,'');
+	});
+
+	it('.maybe() called on number should return that number', function() {
+		var test = F.Maybe(1).maybe('', F.id);
+		assert.equal(test,1);
+	})
+})
+
+describe('Just.bind()', function(){
+	it('.bind called on null should return {}', function() {
+		var test = F.Maybe(null).bind(null);
+		expect(test).to.be.empty;
+	});
+
+	it('.bind called with add1 on 1 should return 2', function() {
+		var test = F.Maybe(1).bind(add1);
+		assert.equal(test.val(), 2);
+	});
+});
+
 
 function add1(x){
 	return x + 1;
