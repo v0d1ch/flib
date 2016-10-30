8449307fc02570b83ee0e637c8642901ffbbf78f
Sasa Bogicevic <bogicevicsasa@yahoo.com>
2016-10-30 16:46:27 +0100

    Add more tests for Nothing

--- test/maybe.test.js
+++ test/maybe.test.js
@@ -1,13 +1,57 @@
 var F = require('../src/flib');
 var chai = require('../node_modules/chai/lib/chai');
 var assert = chai.assert;
+var expect = chai.expect;
 
-it('Nothing - toString should return Nothing', function() {
-  var test = F.Maybe(null).toString();
-  assert.equal(test, 'Nothing');
+describe('Nothing.toString()', function(){
+	it('.toString() if called on null should return Nothing', function() {
+		var test = F.Maybe(null).toString();
+		assert.equal(test, 'Nothing');
+	});
 });
 
-it('Nothing - val() should throw error', function() {
-  var test = F.Maybe(null);
-  assert.throws(test.val,TypeError);
+describe('Nothing.val()', function(){
+	it('.val() if called on null should throw error', function() {
+		var test = F.Maybe(null);
+		assert.throws(test.val,TypeError);
+	});
 });
+
+describe('Nothing.hasValue()', function(){
+	it('.hasValue() called on null should return false', function() {
+		var test = F.Maybe(null);
+		expect(test.hasValue()).to.not.be.true;
+	});
+
+	it('Nothing.hasValue() called on number should return true', function() {
+		var test = F.Maybe(1);
+		expect(test.hasValue()).to.be.true;
+	});
+});
+
+describe('Nothing.maybe()', function(){
+	it('.maybe() called on null should return default value', function() {
+		var test = F.Maybe(null).maybe('', F.id);
+		assert.equal(test,'');
+	});
+	it('Nothing.maybe() called on number should return that number', function() {
+		var test = F.Maybe(1).maybe('', F.id);
+		assert.equal(test,1);
+	})
+})
+
+describe('Nothing.bind()', function(){
+	it('.bind called on null should return {}', function() {
+		var test = F.Maybe(null).bind(null);
+		expect(test).to.be.empty;
+	});
+
+	it('.bind called with add1 on 1 should return 2', function() {
+		var test = F.Maybe(1).bind(add1);
+		assert.equal(test.val(), 2);
+	});
+});
+
+function add1(x){
+	return x + 1;
+}
